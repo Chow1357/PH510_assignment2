483d31c092ea632792f9d50b8b24f9692f21da74
Callum Howie <kwb21127@archie-s.hpc.strath.ac.uk>
2026-02-12 19:17:50 +0000

    included a subtract function in the vector class and tested a few print commands at the bottom

--- part1_code.py
+++ part1_code.py
@@ -18,7 +18,10 @@ class Vector:
 		return f"Vector({self.x}, {self.y}, {self.z})
 
 	def __add__(self, other):
-		return Vector(self.x + other.x,
+		return Vector(self.x + other.x,self.y + other.y, self.z + other.z) # adds two vectors and returns a resulting vector as an instance 
+	
+	def __subtract__(self, other):
+		return Vector((self.x + other.x,self.y + other.y, self.z + other.z)# subtracts two vectors
 	
 	def _magnitude_(self): 
 		return math.sqrt(self.x**2 + self.y**2 + self.z**2)
@@ -27,7 +30,12 @@ class Vector:
 # using the object class 
 #initialise vector with components
 v1 = Vector(1, 2, 3) 
+v2 = vector(4, 5, 6)
 
+v3 = v1 + v2 
+
+print(v1 + v2)
+print(v3.x, v3.y, v3.z)
 print(v1) # prints the vector v1 (1, 2, 3) 
 print("Magnitude:", v1.magnitude()) # computes the magnitude of vector v1
 
