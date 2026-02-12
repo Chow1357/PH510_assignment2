3375a7dc34edc3be4d3e9c11423d860e3ddfb212
Callum Howie <kwb21127@archie-s.hpc.strath.ac.uk>
2026-02-12 19:26:54 +0000

    extra bracket taken out and uppercase V added when definign v1 and v2

--- part1_code.py
+++ part1_code.py
@@ -21,7 +21,7 @@ class Vector:
 		return Vector(self.x + other.x,self.y + other.y, self.z + other.z) # adds two vectors and returns a resulting vector as an instance 
 	
 	def __subtract__(self, other):
-		return Vector((self.x + other.x,self.y + other.y, self.z + other.z)# subtracts two vectors
+		return Vector(self.x + other.x,self.y + other.y, self.z + other.z)# subtracts two vectors
 	
 	def magnitude(self): 
 		return math.sqrt(self.x**2 + self.y**2 + self.z**2)
@@ -30,7 +30,7 @@ class Vector:
 # using the object class 
 #initialise vector with components
 v1 = Vector(1, 2, 3) 
-v2 = vector(4, 5, 6)
+v2 = Vector(4, 5, 6)
 
 v3 = v1 + v2 
 
